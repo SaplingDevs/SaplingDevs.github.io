980467c4a50159fe8ffc42d9346de2a044d14373
TheSilver <94578227+TheSilver1023@users.noreply.github.com>
2025-01-20 08:08:34 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
             rel="noopener noreferrer" 
             className='text-blue-400 hover:text-blue-500 transition duration-300'
           >
-            @TheSilver
+            @TheSilver1023
           </a>
         </footer>
 
