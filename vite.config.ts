96331d67edd81fe64552d88553dee04292c980e3
AlecsDeveloper <alecsdelvalledetorres@gmail.com>
2025-01-21 16:58:17 -0600

    404 again, pls kill me :sob:

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ react() ],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [ { src: '404.html', dest: '' } ]
+    })
+  ],
   build: {
     outDir: 'docs'
   },
